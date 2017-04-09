cce6969dd7750859597f163d0daae09f8a81eaa1
Josh Schonstal <josh@schonstal.org>
2017-04-09 16:42:46 -0700

    ✨ Add basic generators

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,11 +1,26 @@
 import Controller from 'ember-controller';
+import { task, timeout } from 'ember-concurrency';
 
 export default Controller.extend({
   count: 0,
+  generators: 0,
+
+  generate: task(function * () {
+    while (this.get('generators') > 1) {
+      this.incrementProperty('count', this.get('generators') * 0.1);
+      yield timeout(100);
+    }
+  }).drop(),
 
   actions: {
     increment() {
       this.incrementProperty('count');
+    },
+
+    purchase() {
+      this.decrementProperty('count', this.get('generators') * 1.1);
+      this.incrementProperty('generators', 1);
+      this.get('generate').perform();
     }
   }
 });
